edc7a5a13332920abc99e06bbd51e57da9437b02
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-08-19 11:48:03 +0900

    add:readme

--- main.go
+++ main.go
@@ -2,17 +2,41 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	fmt.Println(s)
 }
 
 func main() {
-	go printSomething("This is the first thing to be printed!")
+	var wg sync.WaitGroup
 
-	time.Sleep(1 * time.Second)
+	// 処理は順不同となる
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"gamma",
+		"pi",
+		"zeta",
+		"eta",
+		"theta",
+		"epsilon",
+	}
 
-	printSomething("This is the second thing to be printed!!")
+	// 処理が終わったら、減算される。ゼロにならないようにする。
+	wg.Add(len(words))
+
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
+
+	// ゼロにならないようにする
+	wg.Add(1)
+	printSomething("This is the second thing to be printed!!", &wg)
 }
